071babf94ec365423dfdcd21d26ea12a84e5da8d
Nathan Diggins <eliza.diggins@utah.edu>
2024-02-06 10:12:13 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+
+setup(
+    name="pyrosita",
+    packages=["pyrosita"],
+    version="0.1.0",
+    description="",
+    author="Eliza C. Diggins",
+    author_email="eliza.diggins@utah.edu",
+    url="https://github.com/Wik-Group/pyROSITA",
+    download_url="https://github.com/Wik-Group/pyROSITA/tarball/0.1.0",
+    install_requires=["numpy", "scipy", "yt", "unyt", "cython", "ruamel.yaml", "dill",'astropy','astroquery'],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    include_package_data=True
+)
